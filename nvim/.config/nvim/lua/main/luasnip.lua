f72db51056b6a93cf13a866061c05a97700bbe9c
afa <>
2022-01-08 14:09:08 +0100

    added small test snippets for latex

--- nvim/.config/nvim/lua/main/luasnip.lua
+++ nvim/.config/nvim/lua/main/luasnip.lua
@@ -22,6 +22,9 @@ local fmta = require("luasnip.extras.fmt").fmta
 local types = require("luasnip.util.types")
 local conds = require("luasnip.extras.expand_conditions")
 
+-- helper functions for defining snippets
+local h = {}
+h.paren = function(str) return "{" .. str .. "}" end
 ls.snippets = {
 	-- When trying to expand a snippet, luasnip first searches the tables for
 	-- each filetype specified in 'filetype' followed by 'all'.
@@ -31,18 +34,41 @@ ls.snippets = {
 	--     - luasnip.all
 	-- are searched in that order.
 	all = {
-        s(
-            { trig = "b(%d)", regTrig = true },
-            f(function(_, snip)
-              return "Captured Text: " .. snip.captures[1] .. "."
-            end, {})
-        ),
+        -- snippet to create snippets lol
+       ls.parser.parse_snippet({trig="regsnippet"},
+        [[
       s(
-          { trig = "a%d", regTrig = true },
+          { trig = "$1", regTrig = true },
           f(function(_, snip)
-            return "Triggered with " .. snip.trigger .. "."
+            return $2
           end, {})
-        ),
+      ),
+      $0]]
+      ),
+        -- snippet for ^{} in latex.
+      s(
+          { trig = "(%S+)(%d%d)", regTrig = true },
+          f(function(_, snip)
+            return snip.captures[1] .. "^" .. h.paren(snip.captures[2])
+          end, {})
+      ),
+      s(
+          { trig = "(%S+)(%d)", regTrig = true },
+          f(function(_, snip)
+            return snip.captures[1] .. "^" .. snip.captures[2]
+          end, {})
+      ),      s(
+          { trig = "(%S+)(%d%d)", regTrig = true },
+          f(function(_, snip)
+            return snip.captures[1] .. "_" .. h.paren(snip.captures[2])
+          end, {})
+      ),
+      s(
+          { trig = "(%S+)(%d)", regTrig = true },
+          f(function(_, snip)
+            return snip.captures[1] .. "_" .. snip.captures[2]
+          end, {})
+      ),
   }
 }
 -- 
@@ -50,3 +76,7 @@ ls.snippets = {
 -- Captured Text: 4.
 -- Captured Text: 5.
 -- Captured Text: 0.
+-- Triggered with a5.
+-- Captured Text: 4.
+--
+
