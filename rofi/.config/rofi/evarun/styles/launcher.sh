8dfe3f4d4b24614ef4adc2a7481d88063e156b92
afa <>
2022-02-13 12:09:49 +0100

    changed rofi run theme + alt-tab keyconfig, added web-search fortesting

--- rofi/.config/rofi/evarun/styles/launcher.sh
+++ rofi/.config/rofi/evarun/styles/launcher.sh
@@ -10,20 +10,20 @@
 #
 # style_1     style_2     style_3     style_4     style_5     style_6     style_7
 
+theme="style_2"
 
-dir="$HOME/.config/rofi/evarun"
+dir="$HOME/.config/rofi/launchers/text"
 styles=($(ls -p --hide="colors.rasi" $dir/styles))
 color="${styles[$(( $RANDOM % 10 ))]}"
-color="${styles[0]}"
 
 # comment this line to disable random colors
-# sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
+sed -i -e "s/@import .*/@import \"$color\"/g" $dir/styles/colors.rasi
 
 # comment these lines to disable random style
 themes=($(ls -p --hide="launcher.sh" --hide="styles" $dir))
-theme="${themes[$(( $RANDOM % 2 ))]}"
+theme="${themes[$(( $RANDOM % 7 ))]}"
 
-rofi -no-lazy-grab -show run \
--modi run \
+rofi -no-lazy-grab -show drun \
+-modi run,drun,window \
 -theme $dir/"$theme"
 
