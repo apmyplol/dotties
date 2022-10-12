cfeb4da788ccbb17368cae5691ad3320b507ef72
arthur <>
2022-10-12 23:35:13 +0200

    added plasma autostart variables

---
+++ .config/plasma-workspace/env/vars.sh
@@ -0,0 +1,10 @@
+# export QT_IM_MODULE=Maliit
+# export GTK_IM_MODULE=Maliit
+export MOZ_ENABLE_WAYLAND=1
+export ZDOTDIR=$HOME/.config/zsh
+
+# uncomment to activate fcitx
+# doesnt work with maliit...
+# export GTK_IM_MODULE=fcitx
+# export QT_IM_MODULE=fcitx
+# export XMODIFIERS=@im=fcitx
