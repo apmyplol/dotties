89b5b9fb100110b6b664ac99a8d97969f1de7cba
arthur <>
2022-11-18 14:46:36 +0100

    small changes

--- .config/plasma-workspace/env/vars.sh
+++ .config/plasma-workspace/env/vars.sh
@@ -2,6 +2,7 @@
 # export GTK_IM_MODULE=Maliit
 export MOZ_ENABLE_WAYLAND=1
 export ZDOTDIR=$HOME/.config/zsh
+export QT_QPA_PLATFORM="wayland;xcb"
 
 # uncomment to activate fcitx
 # doesnt work with maliit...
