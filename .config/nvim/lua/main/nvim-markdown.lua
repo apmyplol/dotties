12537a55edf2a825420cc3fb725c3d9bb42b45d1
afa <>
2023-03-24 16:56:38 +0100

    removed nvim directory, adding submodule

--- .config/nvim/lua/main/nvim-markdown.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd "let g:vim_markdown_conceal = 2"
-vim.cmd "let g:vim_markdown_math = 1"
-vim.cmd "let g:vim_markdown_frontmatter = 1"
-
--- .config/nvim/lua/main/obsidian/init.lua
+++
@@ -1,7 +0,0 @@
-
-return {
-  keymaps = require("main.obsidian.obsidian_keymaps"),
-  obsidian = require("main.obsidian.obsidian"),
-  -- syntax = require("main.obsidian.syntax")
-
-}
